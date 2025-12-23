3b8d194726399f171eece55f59cdeaa50e30336e
chaitalimazumder <51844637+chaitalimazumder@users.noreply.github.com>
2025-12-23 22:13:17 +0530

    Change Salesforce deployment target from vscodeOrg to ciOrg(correct alias)

--- scripts/auto_deploy.sh
+++ scripts/auto_deploy.sh
@@ -48,30 +48,9 @@ done <<< "$files"
 # -----------------------------------------------
 echo "=== Deploying to Salesforce ==="
 
-sf project deploy start --target-org vscodeOrg --ignore-conflicts
+sf project deploy start --target-org ciOrg --ignore-conflicts
+
 
 echo "=== DONE: Files Created and Deployment Triggered ==="
 
 
-# '
-# {
-#   "files": [
-#     {
-#       "path": "force-app/main/default/objects/Customer1__c/Customer1__c.object-meta.xml",
-#       "content": "<CustomObject xmlns=\"http://soap.sforce.com/2006/04/metadata\"><label>Customer1</label><pluralLabel>Customers1</pluralLabel><nameField><type>Text</type><label>Customer1 Name</label></nameField><deploymentStatus>Deployed</deploymentStatus><sharingModel>ReadWrite</sharingModel></CustomObject>"
-#     },
-#     {
-#       "path": "force-app/main/default/objects/Customer1__c/fields/Phone__c.field-meta.xml",
-#       "content": "<CustomField xmlns=\"http://soap.sforce.com/2006/04/metadata\"><fullName>Phone__c</fullName><label>Phone</label><type>Phone</type></CustomField>"
-#     },
-#     {
-#       "path": "force-app/main/default/classes/Customer1Helper.cls",
-#       "content": "public class Customer1Helper { public static void hello() { System.debug(\"Hello Customer\"); } }"
-#     },
-#     {
-#       "path": "force-app/main/default/classes/Customer1Helper.cls-meta.xml",
-#       "content": "<ApexClass xmlns=\"http://soap.sforce.com/2006/04/metadata\"><apiVersion>60.0</apiVersion><status>Active</status></ApexClass>"
-#     }
-#   ]
-# }
-# '
